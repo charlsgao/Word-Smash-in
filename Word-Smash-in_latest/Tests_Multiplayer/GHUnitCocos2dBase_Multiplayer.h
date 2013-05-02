511a648aecdd2ab867a06588739f8fc4e68d6df7
Denny Winoto <denny.winoto@gmail.com>
2013-05-01 22:05:52 -0700

    GHUnit Multiplayer test

---
+++ Word-Smash-in_latest/Tests_Multiplayer/GHUnitCocos2dBase_Multiplayer.h
@@ -0,0 +1,13 @@
+//
+//  GHUnitCocos2dBase_Multiplayer.h
+//  SmashingWord_me
+//
+//  Created by Denny Winoto on 5/1/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GHUnitCocos2dBase_Multiplayer : NSObject
+
+@end
---
+++ Word-Smash-in_latest/Tests_Multiplayer/GHUnitCocos2dBase_Multiplayer.m
@@ -0,0 +1,13 @@
+//
+//  GHUnitCocos2dBase_Multiplayer.m
+//  SmashingWord_me
+//
+//  Created by Denny Winoto on 5/1/13.
+//
+//
+
+#import "GHUnitCocos2dBase_Multiplayer.h"
+
+@implementation GHUnitCocos2dBase_Multiplayer
+
+@end
